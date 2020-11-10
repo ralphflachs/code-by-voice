47847008f6c33b83fa9ec400a36ce07304a5cf02
rflachsbart <ralph.flachsbart@cytovale.com>
2020-11-09 16:36:13 -0800

    consolidate

--- extensions/vocola_ext_common.py
+++ extensions/vocola_ext_common.py
@@ -1,4 +1,18 @@
 import os
+import win32clipboard;
+
+# Vocola function: Code.removerhs
+def removerhs():
+	# get clipboard data
+	win32clipboard.OpenClipboard()
+	value = win32clipboard.GetClipboardData()
+	win32clipboard.CloseClipboard()	
+	index = value.find('\=')
+	fullLength = len(value)
+	if (index > 0) :
+		return value[:index-fullLength]
+	else :
+		return value
 
 # Vocola function: Transform.lowercase
 def lowercase(value):
